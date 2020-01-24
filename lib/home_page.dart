e54aeccd553b73e53367786cb1581203135bb5a1
kaustubhKVS <kvshete15@gmail.com>
2020-01-24 23:07:19 +0530

    First Commit

---
+++ lib/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:chatapp/chat_screen.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Donate Chat App"),
+      ),
+
+      body: new ChatScreen(),
+    );
+  }
+}
